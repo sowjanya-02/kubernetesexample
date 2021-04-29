f9797aa7cffe6d015d7ba37c4d666cdc42d6f92d
Sowjanya <44932400+sowjanya-02@users.noreply.github.com>
2021-04-29 13:09:36 +0200

    update app.py

--- app.py
+++ app.py
@@ -2,17 +2,10 @@ from flask import Flask
 #from resources.routes import initialize_routes
 import socket
 app = Flask(__name__)
-#app = create_app()
-#@app.route('/')
-#def hello():
-   #hostname = socket.gethostname()
-   #return 'welcome to conn! store microservice is running on {} pod :)'.format(hostname)
 @app.route('/')
 def home():
-   hostname = socket.gethostname()
-   #local_ip = socket. gethostbyname(localhost)
+   hostname = socket.gethostname(
    return 'welcome to conn! store microservice is running on {} pod :)'.format(hostname)
 
 if __name__ == "__main__":
-      #connservices = "0.0.0.0"
       app.run(host="0.0.0.0", port=9876)
