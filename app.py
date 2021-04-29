cc4191a1cbb2cead4a12f39bdef7ca863bdff854
Sowjanya Gunturu <CC-AEC_EP-0075@configcar.de>
2021-04-29 13:46:31 +0200

    git stash drop

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ from flask import Flask
 #from resources.routes import initialize_routes
 import socket
 app = Flask(__name__)
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 @app.route('/')
 def home():
    hostname = socket.gethostname(
    return 'welcome to conn! store microservice is running on {} pod :)'.format(hostname)
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
       app.run(host="0.0.0.0", port=9876)
+=======
+      app.run(host="0.0.0.0", port=9876)
+>>>>>>> Stashed changes
